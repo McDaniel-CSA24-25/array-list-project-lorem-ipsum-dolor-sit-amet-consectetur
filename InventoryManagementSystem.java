e6eab5e33f479b333a33ef9aa03f74a398d58d04
Zygmondo <randostatistics@gmail.com>
2025-02-28 14:33:30 -0800

    library done, inventory done

--- InventoryManagementSystem.java
+++ InventoryManagementSystem.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 import java.util.ArrayList;
 public class InventoryManagementSystem {
     public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
         ArrayList<Product> products = new ArrayList<>();
         products.add(new Product("Steel Rebar", 0, 10000, 5));
         products.add(new Product("Granite", 1, 2700, 7));
@@ -13,12 +14,34 @@ public class InventoryManagementSystem {
         products.add(new Product("Eraser Cap", 8, 32769, 1));
         products.add(new Product("Peanut", 9, 43046721, 1));
         products.add(new Product());
-        printStocks(products);
+        while (true) {
+            printStocks(products);
+            System.out.println("Do you want to update the inventory? (y/n)");
+            if (keyboard.nextLine().matches("[Yy]")) {
+                while (true) {
+                    System.out.println("Which product do you want to edit? (id)");
+                    int target = keyboard.nextInt();
+                    boolean stop = false;
+                    for (int i = 0; i<products.size(); i++) {
+                        Product p = products.get(i);
+                        if (p.id==target) {
+                            System.out.println("How much of "+p.name+" is there now?");
+                            products.set(i, new Product(p.name, p.id, keyboard.nextInt(), p.price));
+                            stop = true;
+                        }
+                    }
+                    if (stop) {
+                        keyboard.nextLine();
+                        break;
+                    }
+                }
+            } else {break;}
+        }
     }
     public static void printStocks(ArrayList<Product> p) {
         System.out.println("\nInventory:");
         for (Product i : p) {
-            System.out.println(i.id + ": " + i.name + " | " + i.quantity + " units | $" + i.price + "/unit");
+            System.out.println(i.id + ": " + i.name + " | " + i.quantity + " units | $" + i.price + "/unit | $" + i.price*i.quantity + " total" + (i.quantity<5 ? " | LOW STOCK" : ""));
         }
     }
     public static void restock(ArrayList<Product> p) {
